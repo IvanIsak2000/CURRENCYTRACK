0e7b2d5c16df9a9ce0a495db78d487987983aeee
IvanIsak2000 <vingving648@gmail.com>
2023-07-26 19:03:29 +0300

    format on pep8

--- src/main.py
+++ src/main.py
@@ -24,7 +24,13 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def write_in_history(operation_number: int, currency1: str, time1: str, currency2: str, time2: str, different: float) -> None:
+def write_in_history(
+        operation_number: int,
+        currency1: str,
+        time1: str,
+        currency2: str,
+        time2: str,
+        different: float) -> None:
     with open('history_of_currencies.txt', 'a') as file:
         data = f'''
 №{operation_number}:
@@ -35,6 +41,7 @@ Became {currency2} {time2} {different}
         file.write(data)
     logger.info(f'Data with operation number {operation_number} was wrote')
 
+
 def close_program_with_error(name_of_error: str) -> str:
     logger.error(name_of_error)
     console.print(f'{name_of_error}\nPlease reboot program!')
@@ -42,24 +49,24 @@ def close_program_with_error(name_of_error: str) -> str:
 
 def query_execution() -> float:
     '''
-    This function sends a request to the server 
+    This function sends a request to the server
     and parses data from it: currency value
     '''
 
-    first_currency = dpg.get_value('currency_1')    
+    first_currency = dpg.get_value('currency_1')
     second_currency = dpg.get_value('currency_2')
 
     try:
         site_url = 'https://api.freecurrencyapi.com/v1/latest'
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62"}
-        
+
         full_page = requests.get(
             site_url,
             params={
-                'apikey':API_KEY, 
-                'base_currency':first_currency, 
-                'currencies': second_currency}, 
+                'apikey': API_KEY,
+                'base_currency': first_currency,
+                'currencies': second_currency},
             headers=headers)
 
     except requests.exceptions as err:
@@ -75,21 +82,20 @@ def query_execution() -> float:
 
     logger.exception('Server don`t working')
     close_program_with_error('Sorry, server don`t orking')
-    
 
 
 def main_loop() -> str:
-    first_currency = dpg.get_value('currency_1')   
+
+    first_currency = dpg.get_value('currency_1')
     second_currency = dpg.get_value('currency_2')
 
-    if first_currency == second_currency: 
+    if first_currency == second_currency:
         return close_program_with_error('Two identical currencies!')
 
     try:
         time_to_check = float(dpg.get_value('time_to_check'))
     except ValueError:
         return close_program_with_error('Please write time in seconds!')
-
 
     dpg.set_value('info', f"Set as: 1 {first_currency} = {second_currency}")
     logger.info('Start loop')
@@ -120,12 +126,12 @@ def main_loop() -> str:
                                      different]],
                                     tablefmt="simple",
                                     disable_numparse=False))
-            write_in_history(operation_number, 
-            currency_at_the_beginning,
-            current_time1, 
-            currency_after, 
-            current_time2, 
-            different)
+            write_in_history(operation_number,
+                             currency_at_the_beginning,
+                             current_time1,
+                             currency_after,
+                             current_time2,
+                             different)
             logging.info(f'OK, operation  number:{operation_number}')
 
         except Exception as e:
@@ -134,6 +140,7 @@ def main_loop() -> str:
             time.sleep(10)
 
         operation_number += 1
+
 
 dpg.create_context()
 dpg.create_viewport()
@@ -146,14 +153,15 @@ with dpg.window() as main_window:
         dpg.add_listbox(items=list_of_currencies, width=200, tag='currency_2')
 
     dpg.add_text('\n')
-    time_to_check = dpg.add_input_text(label='check time(sec)', tag='time_to_check')
+    time_to_check = dpg.add_input_text(
+        label='check time(sec)', tag='time_to_check')
     dpg.add_button(label='Start', callback=main_loop)
 
     dpg.add_spacer(height=10)
     dpg.add_text('', tag='info')
 
     dpg.add_spacer(height=10)
-    dpg.add_text('', tag='error_message', color=[255,0,0])
+    dpg.add_text('', tag='error_message', color=[255, 0, 0])
 
 
 dpg.set_viewport_title("Currency monitoring",)
